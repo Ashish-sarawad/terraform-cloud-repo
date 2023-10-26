8e9b840ae76b49272341980e280d6d3dcf0e49f4
Ashish-Sarawad <92158835+Ashish-sarawad@users.noreply.github.com>
2023-10-26 13:20:15 +0530

    Create ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.22.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
